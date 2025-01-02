fb268b7ef16a9151078fc343f8851a6ed6354719
Yonatan kps <yonatank@cyncsecure.com>
2025-01-02 15:39:32 +0200

    Refactor ContactSection to use Card component for improved layout and styling.

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,15 +1,11 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
-import grainImage from "@/assets/images/grain.jpg";
+import { Card } from "@/components/Card";
 
 export const ContactSection = () => {
   return (
-    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
+    <section className="py-16 pt-12 lg:py-24 lg:pt-20">
       <div className="container">
-        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
-          <div
-            className="absolute inset-0 opacity-5 -z-10"
-            style={{ backgroundImage: `url(${grainImage.src})` }}
-          ></div>
+        <Card className="p-8 px-10 text-center md:text-left bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900">
           <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
@@ -27,8 +23,8 @@ export const ContactSection = () => {
               </button>
             </div>
           </div>
-        </div>
+        </Card>
       </div>
-    </div>
+    </section>
   );
 };
