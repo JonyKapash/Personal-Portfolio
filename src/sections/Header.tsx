d28dad37da2365c4ce3255244378401e290aa41c
Yonatan kps <yonatank@cyncsecure.com>
2025-03-30 19:40:46 +0300

    new classes to navigation and header; change header position to sticky

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -36,8 +36,8 @@ export const Header = () => {
 
   return (
     <>
-      <div className="flex justify-center items-center fixed top-3 w-full z-10">
-        <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+      <div className="sticky-header">
+        <nav className="nav-container">
           <a
             href="#home"
             onClick={(e) => scrollToSection(e, "home")}
